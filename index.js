c60a77be078bf335f8c77cb54e4091ef25eb5d62
Ivan Kondzerau <konderev@aheadworks.com>
2025-03-28 13:06:44 +0300

    feat: basic CRUD

--- index.js
+++ index.js
@@ -1,54 +1,21 @@
 import express from 'express';
 import cowsay from 'cowsay';
-import { MongoClient } from 'mongodb';
+import { mongoClient } from './db/connection.js';
+import { users } from './routes/users.js';
 
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.MONGO_URL || 'mongodb://master:master123@127.0.0.1:27017/';
 const app = express();
+
 app.use(express.json());
+app.use('/users', users);
 
-const client = new MongoClient(DB_URL);
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
 
-async function connectToDB() {
-    try {
-        await client.connect();
-        console.log(cowsay.say({ text: 'MongoDB connection established' }));
-
-        const db = client.db('ineedhelp');
-
-        //
-        // ToDo: Validation
-        // const test = await db.listCollections({ name: 'some' }).toArray();
-        // console.log(test.length);
-        //
-        
-        const usersCollection = db.collection('users');
-
-        app.get('/users', async (req, res) => {
-            try {
-                const users = await usersCollection.find().toArray();
-                res.json(users);
-            } catch (error) {
-                res.status(500).json({ message: error.message });
-            }
-        });
-
-        app.post('/users', async (req, res) => {
-            try {
-                const user = req.body;
-                await usersCollection.insertOne(user);
-                res.status(201).json(user);
-            } catch (error) {
-                res.status(400).json({ message: error.message });
-            }
-        })
-
-        app.listen(PORT, () => {
-            console.log(`Example app listening on port ${PORT}`)
-        })
-    } catch (error) {
-        console.error(cowsay.say({ text: 'Failed to connect to MongoDB!' }));
-        console.error('Error details: ', error.message);
-    }
-}
-connectToDB();
+process.on('SIGINT', async () => {
+    console.log('SIGINT signal received. Closing MongoDB connection...');
+    await mongoClient.close();
+    console.log(cowsay.say({ text: 'MongoDB connection closed' }));
+    process.exit(0);
+});
